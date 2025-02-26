03fe723e31307b44a0de3f268f73a00995c2d01c
ihsanye <ihsany98@gmail.com>
2025-02-26 12:51:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cookieParser())
+
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
